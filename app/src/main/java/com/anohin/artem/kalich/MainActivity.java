7a308df3ccddcbd601d1b341ef58fc472b1455a6
ArtemMasterJava <aanohin1@gmail.com>
2016-01-10 17:23:19 +0200

    remove unUsed variables

--- app/src/main/java/com/anohin/artem/kalich/MainActivity.java
+++ app/src/main/java/com/anohin/artem/kalich/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     String sign = "";
-    String total = "";
     public Double oneDouble, twoDouble;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
